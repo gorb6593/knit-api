d0cb16f811a181c5d7a0873e93281cf1797e4bcc
harry <gorb6593@gmail.com>
2025-06-17 23:35:37 +0900

    first commit

---
+++ src/main/java/com/knit/api/KnitApiApplication.java
@@ -0,0 +1,13 @@
+package com.knit.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnitApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnitApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/knit/api/KnitApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knit.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnitApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
